cde0e77b9367d8f10a4f9b760c71ddd3840be715
Levi Gleik <leviacedo1@gmail.com>
2019-05-12 21:24:20 -0300

    Importação arquivo CSV.

--- src/kmeans/KMeans.java
+++ src/kmeans/KMeans.java
@@ -5,6 +5,12 @@
  */
 package kmeans;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 /**
  *
  * @author levig
@@ -13,9 +19,32 @@ public class KMeans {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\levig\\Documents\\NetBeansProjects\\KMeans\\src\\kmeans\\agrup_centroides_Q1.csv")))) {
+            String linha = null;
+            Double[][] centroidCSV = new Double[12][5];
+            String[] dadosCentroidCSV;
+            int ic = 0;
+            while ((linha = reader.readLine()) != null) {
+                dadosCentroidCSV = linha.split(",");
+                centroidCSV[ic][0] = Double.parseDouble(dadosCentroidCSV[0]);
+                centroidCSV[ic][1] = Double.parseDouble(dadosCentroidCSV[1]);
+                centroidCSV[ic][2] = Double.parseDouble(dadosCentroidCSV[2]);
+                centroidCSV[ic][3] = Double.parseDouble(dadosCentroidCSV[3]);
+                centroidCSV[ic][4] = Double.parseDouble(dadosCentroidCSV[4]);
+                ic++;
+            }
+            for (int i = 0; i < ic; i++) {
+                System.out.println("I: " + centroidCSV[i][0]);
+                System.out.println("X1: " + centroidCSV[i][1]);
+                System.out.println("X2: " + centroidCSV[i][2]);
+                System.out.println("X3: " + centroidCSV[i][3]);
+                System.out.println("X4: " + centroidCSV[i][4]);
+            }
+        }
+        
     }
     
 }
